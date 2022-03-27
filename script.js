8de450f559018978c5e7780681332fcdc31f9cfd
Nuryana N <nuryan504@gmail.com>
2022-03-27 11:58:46 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+let burger = document.querySelector('.burger');
+let bars1= document.querySelector('.bars1');
+let bars2= document.querySelector('.bars2');
+let bars3= document.querySelector('.bars3');
+let hiddenNav = document.querySelector('.hiddenNav');
+
+burger.addEventListener('click',function(){
+	bars1.classList.toggle('change');
+	bars2.classList.toggle('change');
+	bars3.classList.toggle('change');
+	hiddenNav.classList.toggle('change');
+});
+
+        var typed = new Typed("#typed", {
+            strings: ["Developer", "Designer", "Youtuber", "Freelancer"],
+            typeSpeed: 70,
+            startDelay: 90,
+            loop: true
+        });
